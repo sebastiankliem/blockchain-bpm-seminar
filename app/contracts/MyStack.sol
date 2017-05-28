a141b338c2fdd50f81586e9adfbd7b03410cc3e0
Sebastian Kliem <sebastian-kliem@live.de>
2017-05-28 13:36:54 +0200

    use events instead of manually triggering Stack updates

--- app/contracts/MyStack.sol
+++ app/contracts/MyStack.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.4.0;
 contract MyStack {
     uint[] public stack;
 
+    event StackUpdate(uint[] newStack);
+
     function push(uint number) {
         stack.push(number);
+        StackUpdate(stack);
     }
 
     function pop() {
         delete stack[stack.length - 1];
         stack.length = stack.length - 1;
+        StackUpdate(stack);
     }
 
     function getAll() constant returns (uint[]) {
--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function() {
-  console.log("hallo welt");
-  console.log(MyStack);
-  showStack();
+  // showStack();
 
   $('#button__add_number').on('click', () => {
     addToStack($('#input__add_number').val());
@@ -9,12 +7,17 @@ $(function() {
   $('#button__pop_stack').on('click', () => {
     popStack();
   });
+  MyStack.StackUpdate().then(function(event) {
+    $('#stack_value').text('');
+    event.args.newStack.forEach(function(element) {
+      $('#stack_value').prepend(element + '<br>');
+    }, this);
+  });
 });
 
 function showStack() {
   $('#stack_value').text('');
   MyStack.getAll().then(function(stackValue) {
-    console.log(stackValue);
     stackValue.forEach(function(element) {
       $('#stack_value').prepend(element + '<br>');
     }, this);
@@ -23,13 +26,13 @@ function showStack() {
 
 function addToStack(number) {
   MyStack.push(number).then((transaction) => {
-    showStack();
+    // showStack();
     $('#input__add_number').val('');
   });
 }
 
 function popStack() {
   MyStack.pop().then((transaction) => {
-    showStack();
+    // showStack();
   });
 }
